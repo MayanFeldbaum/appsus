cea3288c411dcf64b856dbce0dbd40b8872d9979
Linoy Alkalay <linoyalkalay@gmail.com>
2022-12-28 19:24:20 +0200

    filter by, css sytle and more

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -10,12 +10,19 @@ export const mailService = {
     get,
     remove,
     save,
-
+    getDefaultFilter
 }
 
-function query() {
+function query(filterBy = getDefaultFilter()) {
     return asyncStorageService.query(MAIL_KEY)
         .then(mails => {
+            if (filterBy.txt) {
+                const regex = new RegExp(filterBy.txt, 'i')
+                mails = mails.filter(mail => {
+                    console.log('mail.body:', mail.body)
+                    return regex.test(mail.subject) || regex.test(mail.body)
+                })
+            }
             // TODO: filterby
             return mails
         })
@@ -37,35 +44,37 @@ function save(mail) {
     }
 }
 
-function getEmptyMail(subject = '', body = '', sentAt = '', to = '') {
+function getEmptyMail(subject = '', body = '', sentAt = '', from = '', to = '') {
     return {
         id: '',
         subject,
         body,
         isRead: utilService.getRandomIntInclusive(0, 1) > 0.4 ? true : false,
         sentAt,
+        removeAt: null,
+        from,
         to
     }
 }
 
 function getDefaultFilter() {
-    return { subject: '', body: '', isRead: false }
+    return { txt: '', isRead: false }
 }
 
 function _createMails() {
     let mails = storageService.loadFromStorage(MAIL_KEY)
     if (!mails || !mails.length) {
         mails = []
-        mails.push(_createMail('Miss you!', 'Would love to catch up sometimes', 1551133930594, 'momo@momo.com'))
-        mails.push(_createMail('Welcome to GitHub', 'You`re the newest member in this community of over 94 million people who use GitHub to host and review code, manage projects, and build software', 1672217852, 'github@github.com'))
-        mails.push(_createMail('Work in Slack with people outside your company', 'Slack Connect is a secure way to communicate with the external people and partners you invite to your Slack instance, and it works just like regular Slack.', 1671651252, 'slackhq@mail.com'))
+        mails.push(_createMail('Miss you!', 'Would love to catch up sometimes', 1551133930594, 'momo@momo.com', 'github@github.com'))
+        mails.push(_createMail('Welcome to GitHub', 'You`re the newest member in this community of over 94 million people who use GitHub to host and review code, manage projects, and build software', 1672217852, 'github@github.com', 'momo@momo.com'))
+        mails.push(_createMail('Work in Slack with people outside your company', 'Slack Connect is a secure way to communicate with the external people and partners you invite to your Slack instance, and it works just like regular Slack.', 1671651252, 'momo@momo.com', 'slackhq@mail.com'))
         console.log('mails:', mails)
         storageService.saveToStorage(MAIL_KEY, mails)
     }
 }
 
-function _createMail(subject, body, sentAt, to) {
-    const mail = getEmptyMail(subject, body, sentAt, to)
+function _createMail(subject, body, sentAt, from, to) {
+    const mail = getEmptyMail(subject, body, sentAt, from, to)
     mail.id = utilService.makeId()
     return mail
 }
